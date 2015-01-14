6b94275cb6af6b24a337c9a924f86d5f45078d73
jciberta <josep.ciberta@gmail.com>
2015-01-14 18:22:28 +0100

    ReplaceSpecialChars function

--- LibStrUtils.pas
+++ LibStrUtils.pas
@@ -5,9 +5,38 @@ interface
 uses
     Classes, SysUtils;
 
+function ReplaceSpecialChars(sText: string): string;
 function Split(sText: string; cDelimiter: Char): TStringList;
 
 implementation
+
+function ReplaceSpecialChars(sText: string): string;
+var
+    i: integer;
+begin
+    for i:= 1 to Length(sText) do
+    begin
+        case (sText[i]) of
+            // Vowels
+            'À', 'Á', 'Ä', 'Â': sText[i] := 'A';
+            'È', 'É', 'Ë', 'Ê': sText[i] := 'E';
+            'Ì', 'Í', 'Ï', 'Î': sText[i] := 'I';
+            'Ò', 'Ó', 'Ö', 'Ô': sText[i] := 'O';
+            'Ù', 'Ú', 'Ü', 'Û': sText[i] := 'U';
+            'à', 'á', 'ä', 'â': sText[i] := 'a';
+            'è', 'é', 'ë', 'ê': sText[i] := 'e';
+            'ì', 'í', 'ï', 'î': sText[i] := 'i';
+            'ò', 'ó', 'ö', 'ô': sText[i] := 'o';
+            'ù', 'ú', 'ü', 'û': sText[i] := 'u';
+            // Consonants
+            'Ç': sText[i] := 'C';
+            'ç': sText[i] := 'c';
+            'Ñ': sText[i] := 'N';
+            'ñ': sText[i] := 'n';
+        end;
+    end;
+    result := sText;
+end;
 
 function Split(sText: string; cDelimiter: Char): TStringList;
 var
--- Test/TestCases.pas
+++ Test/TestCases.pas
@@ -8,6 +8,7 @@ uses
 type
     TTestLibStrUtils = class(TTestCase)
     published
+        procedure TestReplaceSpecialChars;
         procedure TestSplit;
     end;
 
@@ -15,16 +16,22 @@ implementation
 
 uses Classes, LibStrUtils;
 
+procedure TTestLibStrUtils.TestReplaceSpecialChars;
+begin
+    Check(ReplaceSpecialChars('Français') = 'Francais', 'Error in ReplaceSpecialChars 1');
+    Check(ReplaceSpecialChars('Cafè') = 'Cafe', 'Error in ReplaceSpecialChars 2');
+end;
+
 procedure TTestLibStrUtils.TestSplit;
 var
     sl: TStringList;
 begin
-    Check(Split('Peter Edward Smith', #32)[0] = 'Peter', 'Error in Split');
-    Check(Split('Peter Edward Smith', #32)[1] = 'Edward', 'Error in Split');
-    Check(Split('Peter Edward Smith', #32)[2] = 'Smith', 'Error in Split');
-    Check(Split('Peter,Edward,Smith', ',')[0] = 'Peter', 'Error in Split');
-    Check(Split('Peter:Edward,Smith', ':')[0] = 'Peter', 'Error in Split');
-    Check(Split('Peter:Edward,Smith', ',')[0] = 'Peter:Edward', 'Error in Split');
+    Check(Split('Peter Edward Smith', #32)[0] = 'Peter', 'Error in Split 1');
+    Check(Split('Peter Edward Smith', #32)[1] = 'Edward', 'Error in Split 2');
+    Check(Split('Peter Edward Smith', #32)[2] = 'Smith', 'Error in Split 3');
+    Check(Split('Peter,Edward,Smith', ',')[0] = 'Peter', 'Error in Split 4');
+    Check(Split('Peter:Edward,Smith', ':')[0] = 'Peter', 'Error in Split 5');
+    Check(Split('Peter:Edward,Smith', ',')[0] = 'Peter:Edward', 'Error in Split 6');
 end;
 
 initialization
